76c0d3eefa0fd00291ad50f4f0f5422a8c98e0f6
Mrigaj <mrigs2429@gmail.com>
2026-07-04 19:23:29 +0530

    Add requirements.txt and configure port for deployment

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
+import os
+import uvicorn
+
 app=FastAPI()
+
 @app.get("/")
 def  home():
     return{"message":"Hello F1"}
+
+if __name__  == "__main__":
+    port=int(os.environ.get("PORT",8000))
+    uvicorn.run("main:app",host="0.0.0.0",port=port)
+    
